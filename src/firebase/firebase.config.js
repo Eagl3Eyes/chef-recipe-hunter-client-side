087a0ffdefb7d8127fbfa133e90687e1b51471d6
Eagl3Eyes <58110053+Eagl3Eyes@users.noreply.github.com>
2023-05-04 14:01:18 +0600

    firebase setup done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATv49SqFX63-uInqM05svS-ob6P5559e4",
+  authDomain: "chef-recipe-hunter-278ef.firebaseapp.com",
+  projectId: "chef-recipe-hunter-278ef",
+  storageBucket: "chef-recipe-hunter-278ef.appspot.com",
+  messagingSenderId: "29479317228",
+  appId: "1:29479317228:web:67929a3abaa3e6355fbfc9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
