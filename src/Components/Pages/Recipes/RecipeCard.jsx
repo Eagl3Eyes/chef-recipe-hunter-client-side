40dcaa013d278871f57213909e1bc3b92c3394bd
Eagl3Eyes <58110053+Eagl3Eyes@users.noreply.github.com>
2023-05-04 20:19:13 +0600

    recipes fixed

--- src/Components/Pages/Recipes/RecipeCard.jsx
+++ src/Components/Pages/Recipes/RecipeCard.jsx
@@ -5,68 +5,61 @@ import { Rating } from "@smastrom/react-rating";
 import "@smastrom/react-rating/style.css";
 
 const RecipeCard = ({ recipe }) => {
-  const [isDisabled, setIsDisabled] = useState(false);
+    const [isDisabled, setIsDisabled] = useState(false);
 
-  const { id, details, name, rating, ingredients, thumbnail, cookingMethod } =
-    recipe;
-  const handleFavourite = () => {
-    toast.success("Added to favourite!");
-    setIsDisabled(true);
-  };
-  return (
-    <>
-      <div className="card pb-10 px-4 card-compact w-full bg-base-100 overflow-hidden shadow-xl">
-        <figure>
-          <img className="h-72 w-full object-cover" src={thumbnail} alt="recipe" />
-        </figure>
-        <div className="card">
-          <h2 className="card-title mt-5 text-2xl">{name}</h2>
-          <p>{details}</p>
-          
-          <div className="">
-            <div>
-              <h3 className="font-bold text-xl my-4">Ingredients:</h3>
-              {ingredients ? (
-                <>
-                  {ingredients.map((i) => (
-                    <p>{i}</p>
-                  ))}
-                </>
-              ) : (
-                ""
-              )}
+    const { id, details, name, rating, ingredients, thumbnail, cookingMethod } =
+        recipe;
+    const handleFavourite = () => {
+        toast.success("Added to favourite!");
+        setIsDisabled(true);
+    };
+    return (
+        <div className="card pb-10 card-compact w-full bg-base-100 overflow-hidden shadow-xl">
+            <figure>
+                <img className="h-72 w-full object-cover" src={thumbnail} alt="recipe" />
+            </figure>
+            <div className="card ml-5 ">
+                <h2 className="card-title mt-5 text-2xl">{name}</h2>
+                <p>{details}</p>
+
+                <div className="">
+                    <div>
+                        <h3 className="font-bold text-xl my-4">Ingredients:</h3>
+                        {ingredients ? (
+                            <>
+                                {ingredients.map((i) => (
+                                    <p>{i}</p>
+                                ))}
+                            </>
+                        ) : (
+                            ""
+                        )}
+                    </div>
+                    <br />
+                    <p>
+                        <span className="text-xl font-bold ">Cooking Methode: </span>
+                        <span className="text-gray-700">{cookingMethod}</span>
+                    </p>
+                </div>
+
+                <div className="mr-auto flex items-center mt-4 mb-6">
+                    <Rating
+                        style={{ maxWidth: 100 }}
+                        value={rating}
+                        readOnly
+                    ></Rating>
+                    {rating}
+                </div>
             </div>
-            <p>
-              <span className="text-xl font-bold ">Cooking Methode:</span>
-              {cookingMethod}
-            </p>
-          </div>
-
-          <div className="">
-          <div className="flex card-actions justify-end gap-5">
             <button
-              onClick={handleFavourite}
-              disabled={isDisabled}
-              className="btn btn-primary"
+                onClick={handleFavourite}
+                disabled={isDisabled}
+                className="btn btn-primary absolute inset-x-0 bottom-0"
             >
-              Favourite
+                Favourite
             </button>
-            <div className="mr-auto flex items-center my-auto ">
-              <Rating
-                style={{ maxWidth: 100 }}
-                value={rating}
-                readOnly
-              ></Rating>
-              {rating}
-            </div>
-          </div>
-          </div>
         </div>
-      </div>
-
-     
-    </>
-  );
+    );
 };
 
 export default RecipeCard;
--- src/Components/Pages/Recipes/Recipes.jsx
+++ src/Components/Pages/Recipes/Recipes.jsx
@@ -5,55 +5,51 @@ import "react-toastify/dist/ReactToastify.css";
 import { FaThumbsUp } from "react-icons/fa";
 
 const Recipes = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
 
-  const chef = useLoaderData();
-  const chefRecipes = chef.recipes;
+    const chef = useLoaderData();
+    const chefRecipes = chef.recipes;
 
-  return (
-    <div >
-      <div className="w-8/12 mx-auto text-center">
-        <div className="w-64 h-64 rounded-full overflow-hidden mx-auto my-4">
-          <img
-            className="w-full h-full object-cover"
-            src={chef?.chefPicture}
-            alt="chef image"
-          />
+    return (
+        <div >
+            <div className="w-8/12 mx-auto text-center">
+                <div className="w-64 h-64 rounded-full overflow-hidden mx-auto my-4">
+                    <img
+                        className="w-full h-full object-cover"
+                        src={chef?.chefPicture}
+                        alt="chef image"
+                    />
+                </div>
+                <div className="md:w-10/12 mx-auto mb-10">
+                    <h2 className="text-4xl my-9 font-bold">{chef?.chefName}
+                        <div className="badge badge-outline p-3 ml-5 h-5 gap-2 ">
+                            <FaThumbsUp></FaThumbsUp>{chef?.likes}</div>
+                    </h2>
+
+                    <p className="text-gray-500">
+                        {chef?.chefBio}
+                    </p>
+                    <p className="font-semibold mt-4">
+                        {chef?.numberOfRecipes} Recipes
+                    </p>
+                    <p className="font-semibold">
+                        {chef?.yearsOfExperience} Years Experience
+                    </p>
+                </div>
+            </div>
+            <hr />
+            <div>
+                <h2 className="font-bold text-4xl text-center my-10 ">My Recipes</h2>
+                <div className="md:grid grid-cols-2 gap-10 mx-auto w-8/12">
+                    {chefRecipes.map((recipe) => (
+                        <RecipeCard key={recipe._id} recipe={recipe}></RecipeCard>
+                    ))}
+                </div>
+            </div>
         </div>
-        <div className="md:w-10/12 mx-auto mb-10">
-          <h2 className="text-5xl my-9 font-bold">{chef?.chefName} 
-          <div className="badge badge-outline p-3 ml-5 h-5 gap-2 ">
-          <FaThumbsUp></FaThumbsUp>{chef?.likes}</div>
-          </h2>
-          
-          
-          <p>
-            <span className="font-bold">About: </span>
-            {chef?.chefBio}
-          </p>
-          <p className="font-semibold mt-4">
-            I have <span className="font-bold">{chef?.numberOfRecipes}</span>{" "}
-            recipes
-          </p>
-          <p className="font-semibold">
-            My Experience in this profession is{" "}
-            <span className="font-bold"> {chef?.yearsOfExperience}</span> years
-          </p>
-        </div>
-      </div>
-      <hr />
-      <div>
-        <h2 className="font-bold text-4xl text-center my-10 ">My Recipes</h2>
-        <div className="md:grid grid-cols-2 gap-10 mx-auto w-10/12">
-          {chefRecipes.map((recipe) => (
-            <RecipeCard key={recipe._id} recipe={recipe}></RecipeCard>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Recipes;
