6c714ea8780baac6186727784343d75bbf194a44
Eagl3Eyes <58110053+Eagl3Eyes@users.noreply.github.com>
2023-05-05 02:35:57 +0600

    bug fixed

--- src/Components/Layout/Main.jsx
+++ src/Components/Layout/Main.jsx
@@ -3,13 +3,14 @@ import Navbar from '../Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
-            <div>
+            <div className="mt-auto mx-auto">
                 <Footer></Footer>
                 <ToastContainer></ToastContainer>
             </div>
